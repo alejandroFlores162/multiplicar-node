79953ca34202295b625d557d3b53acb244d9e737
Alejandro Flores Castillo <AFloresCa@zurich.mx>
2020-05-14 19:47:50 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un numero`);
+            return;
+        }
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla del ${base}.txt`);
+        });
+    });
+}
+
+let listar = (base, limite) => {
+    console.log(`tabla de ${base}`.green)
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base * i}\n`)
+
+    }
+}
+
+module.exports = {
+    crearArchivo,
+    listar
+}
